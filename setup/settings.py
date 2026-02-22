705257fb63dc56fec8a42ed4197813982589cd7e
Sergio Graca <sergiofgraca@gmail.com>
2026-02-22 20:48:29 -0300

    fix allowed hosts

--- setup/settings.py
+++ setup/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret-key')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['inspire-mois.onrender.com']
 
 
 # ==================================================
