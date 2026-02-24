eadacafca2a9758ad0e8358ab6fb3a66a5f088b6
Sergio Graca <sergiofgraca@gmail.com>
2026-02-23 21:52:29 -0300

    criando migrations do app todos

--- setup/settings.py
+++ setup/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ==================================================
 SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret-key')
 
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['*']
 
